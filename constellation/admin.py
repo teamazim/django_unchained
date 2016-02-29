c193393c6c61e6aa67ea248e24e2e8ad568175f6
shaneIRL <113401972@umail.ucc.ie>
2016-02-29 16:30:09 +0000

    Database
    Put the tables I could think of in the models.py file and linked them up
    in the admin.py file. Renamed the "UserProfile" model that was there as
    "UserProfile1" to show what was there. Any suggestions before we
    finalise this?

--- constellation/admin.py
+++ constellation/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import UserProfiles, Events, Notes, Venues, Counties, Tickets, Bookings
+
+admin.site.register(UserProfiles)
+admin.site.register(Events)
+admin.site.register(Notes)
+admin.site.register(Venues)
+admin.site.register(Counties)
+admin.site.register(Tickets)
+admin.site.register(Bookings)
--- constellation/models.py
+++ constellation/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User 
 # Create your models here.
 
-class UserProfile(models.Model):
+class UserProfile1(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	first_name = models.CharField( max_length=40, default='test')
 	last_name = models.CharField( max_length=40, default='test' )
@@ -12,3 +12,109 @@ class UserProfile(models.Model):
 	addr_line_2 = models.CharField( max_length=120, default='test' )
 	county = models.CharField( max_length=40, default='test' )
 	country = models.CharField( max_length=40, default='test' )
+
+COUNTIES = (
+    ('antrim', 'Antrim'),
+    ('armagh', 'Armagh'),
+    ('carlow', 'Carlow'),
+    ('cavan', 'Cavan'),
+    ('clare', 'Clare'),
+    ('cork', 'Cork'),
+    ('derry', 'Derry'),
+    ('donegal', 'Donegal'),
+    ('down', 'Down'),
+    ('dublin', 'Dublin'),
+    ('fermanagh', 'Fermanagh'),
+    ('galway', 'Galway'),
+    ('kerry', 'Kerry'),
+    ('kildare', 'Kildare'),
+    ('kilkenny', 'Kilkenny'),
+    ('laois', 'Laois'),
+    ('leitrim', 'Leitrim'),
+    ('limerick', 'Limerick'),
+    ('longford', 'Longford'),
+    ('louth', 'Louth'),
+    ('mayo', 'Mayo'),
+    ('meath', 'Meath'),
+    ('monaghan', 'Monaghan'),
+    ('offaly', 'Offaly'),
+    ('roscommon', 'Roscommon'),
+    ('sligo', 'Sligo'),
+    ('tipperary', 'Tipperary'),
+    ('tyrone', 'Tyrone'),
+    ('waterford', 'Waterford'),
+    ('westmeath', 'Westmeath'),
+    ('wexford', 'Wexford'),
+    ('wicklow', 'Wicklow'),
+)
+
+class UserProfiles(models.Model):
+	GENDERS = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+	)
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	userID = models.AutoField(primary_key=True)
+	dob = models.DateField()
+	userAddress = models.TextField()
+	userCounty = models.CharField(max_length = 9, choices=COUNTIES)
+	userCountry = models.CharField(max_length = 50)
+	phone = models.CharField(max_length = 20)
+	gender = models.CharField(max_length=1, choices=GENDERS)
+	language = models.CharField(max_length = 50)
+
+class Events(models.Model):
+	SEASONS = (
+        ('spring', 'Spring'),
+        ('summer', 'Summer'),
+        ('autumn', 'Autumn'),
+        ('winter', 'Winter'),
+	)
+	eventID = models.AutoField(primary_key=True)
+	title = models.CharField(max_length = 50)
+	eventDescription = models.TextField()
+	startTime = models.TimeField()
+	endTime = models.TimeField()
+	startDate = models.DateField()
+	endDate = models.DateField()
+	venueID = models.IntegerField()
+	season = models.CharField(max_length=6, choices=SEASONS)
+	notesID = models.IntegerField()
+	availability = models.IntegerField()
+
+class Notes(models.Model):
+	notesID = models.AutoField(primary_key=True)
+	fileName = models.CharField(max_length = 50)
+	dataType = models.CharField(max_length = 10)
+	noteDescription = models.TextField()
+	dateAdded = models.DateField()
+
+class Venues(models.Model):
+	VENUE_TYPES = (
+        ('indoor', 'Indoor'),
+        ('outdoor', 'Outdoor'),
+	)
+	venueID = models.AutoField(primary_key=True)
+	venueAddress = models.TextField()
+	venueCounty = models.CharField(max_length = 9, choices=COUNTIES)
+	venueCountry = models.CharField(max_length = 50)
+	capacity = models.PositiveIntegerField()
+	venueType = models.CharField(max_length=7, choices=VENUE_TYPES)
+
+class Counties(models.Model):
+	PROVINCES = (
+        ('connacht', 'Connacht'),
+        ('leinster', 'Leinster'),
+        ('munster', 'Munster'),
+        ('ulster', 'Ulster'),
+	)
+	countyID = models.AutoField(primary_key=True)
+	countyName = models.CharField(max_length=9, choices=COUNTIES)
+	province = models.CharField(max_length=8, choices=PROVINCES)
+
+class Bookings(models.Model):
+	bookingID = models.AutoField(primary_key=True)
+	userID = models.IntegerField()
+	eventID = models.IntegerField()
+	qrCode = models.CharField(max_length = 20)
+	confirmed = models.BooleanField()
