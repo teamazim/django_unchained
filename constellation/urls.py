8cdb1bb358e1680bbc1cf6065d8dfde991e9b3f5
teamazim <teamazim829@gmail.com>
2016-03-07 18:59:47 +0000

    put back in url

--- constellation/urls.py
+++ constellation/urls.py
@@ -8,7 +8,10 @@ from django.conf import settings
 from django.conf.urls import include, patterns, url
 
 urlpatterns = [
-		
+	
+	#localhost:8000/constellation
+	url(r'^$', views.index, name='index'),	
+ 
 	#localhost:8000/constellation/login 
 	url(r'^login/$', views.Login, name='Login'),
 	
